45e760b7ac53b1d14a23a3d4835ca087b09ebf81
SleepySammie <d0v3e1@gmail.com>
2026-08-26 16:04:42 -0600

    Test commit

--- Module1/assignment1/ooplab.py
+++ Module1/assignment1/ooplab.py
@@ -1,36 +1,12 @@
-# student1_name = "Aisha"
-# student1_gpa = 3.8
- 
-# student2_name = "Diego"
-# student2_gpa = 3.2
- 
-# def print_student(name, gpa):
-#     print(f"{name}: GPA {gpa}")
- 
-# print_student(student1_name, student1_gpa)
-# print_student(student2_name, student2_gpa)
+
  
 class Student:
     def __init__(self, name, gpa):
         self.name = name
         self.gpa = gpa
 
-students = [
-    Student('Aisha', 3.2),
-    Student('Diego', 3.8),
-    Student('Steve', 1.5)
-]
+    def getName(self):
+        return self.name
 
-avg = 0.0
-total = 0.0
-for s in students:
-    total += s.gpa
-
-print(f'The average is {total/len(students):.2f}')
-
-
-# s1 = Student('Aisha',3.8)
-# s2 = Student('Diego',3.2)
-
-# print(f'Name: {s1.name} Gpa: {s1.gpa}')
-# print(f'Name: {s2.name} Gpa: {s2.gpa}')
+    def getGpa(self):
+        return self.gpa
