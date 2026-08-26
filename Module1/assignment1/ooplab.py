23c868577360eb7fca4c7cb9891205d396684820
SleepySammie <d0v3e1@gmail.com>
2026-08-26 17:00:40 -0600

    Completed Lab

--- Module1/assignment1/ooplab.py
+++ Module1/assignment1/ooplab.py
@@ -8,5 +8,25 @@ class Student:
     def getName(self):
         return self.name
 
+    def setName(self, newName):
+        self.name = newName
+
     def getGpa(self):
         return self.gpa
+
+    def setGpa(self, newGpa):
+        if newGpa > 4.0 or newGpa < 0.0:
+            print('Invalid new gpa')
+        else:
+            self.gpa = newGpa
+
+def main():
+    s1 = Student('Diego', 3.2)
+
+    s1.setGpa(5.5) # test bad value
+    s1.setGpa(3.0)
+
+    print(s1.getName())
+    print(s1.getGpa())
+
+main()
