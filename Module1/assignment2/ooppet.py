cbcc13741018b680ab4eddd92cba85639f8f36aa
SleepySammie <d0v3e1@gmail.com>
2026-08-26 15:11:10 -0600

    Completed ooppet.py, added comments for enhanced readability

--- Module1/assignment2/ooppet.py
+++ Module1/assignment2/ooppet.py
@@ -1,17 +1,21 @@
+# Made a Pet class that has name, species, and age as parameters
 class Pet:
     def __init__(self, name, species, age):
         self.name = name
         self.species = species
         self.age = age
 
+    # Made the describe method that returns an f string containing the name, age, and species.
     def describe(self):
         return (f'{self.name} is a {self.age}-year-old-{self.species}')
-
+    
+# Made the pets list
 pets = [
     Pet('Rex','Dog', 3),
     Pet('Luna','Cat', 2),
     Pet('Kiwi', 'Parrot', 1)
 ]
 
+# Made a for loop that iterates over pets, then calls the describe method to print the information
 for p in pets:
     print(p.describe())
