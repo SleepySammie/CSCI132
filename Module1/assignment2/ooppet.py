9afb1b64cac5b6d9c9537ab667087eb98b24c7f5
SleepySammie <d0v3e1@gmail.com>
2026-08-26 14:47:19 -0600

    Created a list for the Pet class. Created code to iterate over pets list and call the describe() method.

--- Module1/assignment2/ooppet.py
+++ Module1/assignment2/ooppet.py
@@ -4,11 +4,14 @@ class Pet:
         self.species = species
         self.age = age
 
-    def descibe(self):
+    def describe(self):
         return (f'{self.name} is a {self.age}-year-old-{self.species}')
 
+pets = [
+    Pet('Rex','Dog', 3),
+    Pet('Luna','Cat', 2),
+    Pet('Kiwi', 'Parrot', 1)
+]
 
-pet1 = Pet('Rex','Dog',3)
-pet2 = Pet('Luna','Cat',2)
-pet3 = Pet('Kiwi' 'Parrot',1)
-
+for p in pets:
+    print(p.describe())
