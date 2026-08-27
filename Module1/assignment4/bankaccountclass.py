2a87c492332d71056b00c752230d902a42a7991a
SleepySammie <d0v3e1@gmail.com>
2026-08-26 20:40:39 -0600

    Created deposit and getBalance methods. Created main()

--- Module1/assignment4/bankaccountclass.py
+++ Module1/assignment4/bankaccountclass.py
@@ -2,3 +2,19 @@ class BankAccount:
     def __init__(self, ownerName):
         self.ownerName = ownerName
         self.balance = 0
+
+    def deposit(self, amount):
+        if amount < 0 or amount == 0:
+            print('Error- Deposit must be a positive amount, balance unchanged')
+        else:
+            self.balance += amount
+
+    def getBalance(self):
+        return self.balance 
+    
+def main():
+    account = BankAccount('Priya')
+    account.deposit(100)
+    print(account.getBalance())
+
+main()
