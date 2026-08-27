4e68f87faa4d3f7a72975ff18c0b1028193cbc29
SleepySammie <d0v3e1@gmail.com>
2026-08-26 21:00:43 -0600

    Created withdraw method called functions in main as specified by assignment. Increased readability of output.

--- Module1/assignment4/bankaccountclass.py
+++ Module1/assignment4/bankaccountclass.py
@@ -9,12 +9,21 @@ class BankAccount:
         else:
             self.balance += amount
 
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print('Error- insufficient funds, balance unchanged')
+        else:
+            self.balance -= amount
+
     def getBalance(self):
-        return self.balance 
+        return (f'Balance: {self.balance}') 
     
 def main():
     account = BankAccount('Priya')
     account.deposit(100)
+    account.withdraw(30)
+    account.withdraw(1000)
+    account.deposit(-5)
     print(account.getBalance())
 
 main()
