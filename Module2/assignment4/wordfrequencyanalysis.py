3e1cd8242740520f2d5273a829074f38d0f4f22c
SleepySammie <d0v3e1@gmail.com>
2026-09-03 17:34:10 -0600

    Refactored word frequency analysis to include cleanText and textList processing

--- Module2/assignment4/wordfrequencyanalysis.py
+++ Module2/assignment4/wordfrequencyanalysis.py
@@ -3,5 +3,14 @@ import string
 # Opens gba.txt in read mode, creates rawText variable to store file, calls casefold to make every word lowercase.
 with open('gba.txt', 'r') as file:
     rawText = file.read().casefold()
-    print(rawText)
 
+    # Created cleanText variable, calls translate method to replace text, creates translation table with empty arguments and removes punctuation using the third argument 
+    cleanText = rawText.translate(str.maketrans('', '', string.punctuation))
+    cleanText.split()
+    print(cleanText)
+
+    # Created textList and split cleanText into individual words in the list.
+    textList = cleanText.split()
+    print(textList)
+
+    
