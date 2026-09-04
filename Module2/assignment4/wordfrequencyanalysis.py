21ee7ebf1013563cdcebfcd1939a5fa1fef25d66
SleepySammie <d0v3e1@gmail.com>
2026-09-03 20:24:50 -0600

    Added getTextList, getTextDictionary and main functions

--- Module2/assignment4/wordfrequencyanalysis.py
+++ Module2/assignment4/wordfrequencyanalysis.py
@@ -1,16 +1,34 @@
 import string
 
-# Opens gba.txt in read mode, creates rawText variable to store file, calls casefold to make every word lowercase.
-with open('gba.txt', 'r') as file:
-    rawText = file.read().casefold()
+def getTextList():
+    # Opens gba.txt in read mode, creates rawText variable to store file, calls casefold to make every word lowercase.
+    with open('gba.txt', 'r') as file:
+        rawText = file.read().casefold()
 
-    # Created cleanText variable, calls translate method to replace text, creates translation table with empty arguments and removes punctuation using the third argument 
-    cleanText = rawText.translate(str.maketrans('', '', string.punctuation))
-    cleanText.split()
-    print(cleanText)
+        # Created cleanText variable, calls translate method to replace text, creates translation table with empty arguments and removes punctuation using the third argument.
+        cleanText = rawText.translate(str.maketrans('', '', string.punctuation))
+        cleanText.split()
 
-    # Created textList and split cleanText into individual words in the list.
-    textList = cleanText.split()
-    print(textList)
+        # Created textList and split cleanText into individual words in the list.
+        textList = cleanText.split()
+        return (textList)
+
+
+def getTextDictionary():
+    # Created an empty dictionary and iterated over the list, assigned the words to the dictionary as a key, and set the value to 1.
+    textDict = {}
+    for word in getTextList():
+        textDict[word] = 1 
+
+    return (textDict)
 
     
+
+
+def main():
+    # Print statement for testing.
+    Count = getTextDictionary()
+    print(Count)
+
+
+main()
