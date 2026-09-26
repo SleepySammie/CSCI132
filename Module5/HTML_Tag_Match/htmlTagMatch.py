1674bbb6aee42cae16f7d37e5ac4b6f895d1a947
SleepySammie <d0v3e1@gmail.com>
2026-09-25 22:41:17 -0600

    implement tag checking logic in checkTags function

--- Module5/HTML_Tag_Match/htmlTagMatch.py
+++ Module5/HTML_Tag_Match/htmlTagMatch.py
@@ -1,4 +1,10 @@
 from pythonds3.basic import Stack
 
 def checkTags(htmlStr):
-    
+    s = Stack()
+    for tag in htmlStr:
+        if tag in "<html>" or tag in "<head>" or tag in "<h1>" or tag in "<body>" or tag in "<title>":
+            s.push(tag)
+        else:
+            if s.is_empty():
+                return False
